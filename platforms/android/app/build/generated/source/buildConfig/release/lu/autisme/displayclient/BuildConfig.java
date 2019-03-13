977a71ec301386f4a00d05d3ae72213672ee7a8d
trineon89 <trineon89@gmail.com>
2019-03-13 16:22:40 +0100

    +

--- platforms/android/app/build/generated/source/buildConfig/release/lu/autisme/displayclient/BuildConfig.java
+++ platforms/android/app/build/generated/source/buildConfig/release/lu/autisme/displayclient/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "lu.autisme.displayclient";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 1000;
-  public static final String VERSION_NAME = "0.10.0";
+  public static final int VERSION_CODE = 1001;
+  public static final String VERSION_NAME = "0.10.1";
 }
